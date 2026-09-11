15b89b2e94eb91151f52701606c0bfa0c4a07089
BigPolarBear <hanxincade@gmail.com>
2026-09-11 08:08:18 +0800

    style: preserve TokenFactory ABI declaration order

--- interfaces/core/ILOVE20TokenFactory.sol
+++ interfaces/core/ILOVE20TokenFactory.sol
@@ -2,6 +2,20 @@
 pragma solidity =0.8.17;
 
 interface ILOVE20TokenFactory {
+    error AlreadyInitialized();
+    error InvalidAddress();
+    error EmptyString();
+    error InvalidSupply();
+    error UnauthorizedCaller();
+
+    event TokenCreated(
+        address indexed tokenAddress,
+        address indexed parentTokenAddress,
+        string name,
+        string symbol,
+        address distributor
+    );
+
     function init(
         address pairFactoryAddress,
         address launchAddress,
@@ -20,18 +34,4 @@ interface ILOVE20TokenFactory {
     function mintAddress() external view returns (address);
     function initialSupply() external view returns (uint256);
     function maxSupply() external view returns (uint256);
-
-    event TokenCreated(
-        address indexed tokenAddress,
-        address indexed parentTokenAddress,
-        string name,
-        string symbol,
-        address distributor
-    );
-
-    error AlreadyInitialized();
-    error InvalidAddress();
-    error EmptyString();
-    error InvalidSupply();
-    error UnauthorizedCaller();
 }
